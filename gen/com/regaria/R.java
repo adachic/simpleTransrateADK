d70aae941ff3cad9517e0b006b92b5ac47a09c70
Akinori ADACHI <adachi.c.akinori@gmail.com>
2012-12-16 14:48:06 +0900

    バトル状態、移動状態、索敵状態追加。 カメラ補正、移動実装。

--- gen/com/regaria/R.java
+++ gen/com/regaria/R.java
@@ -13,11 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int button_off=0x7f060002;
+        public static final int button_on=0x7f060001;
+        public static final int textView1=0x7f060000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+    }
+    public static final class xml {
+        public static final int accessory_filter=0x7f040000;
     }
 }
--- src/com/regaria/simpleTransrateADKActivity.java
+++ src/com/regaria/simpleTransrateADKActivity.java
@@ -1,13 +1,182 @@
 package com.regaria;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import		 java.io.IOException;
 
-public class simpleTransrateADKActivity extends Activity {
-    /** Called when the activity is first created. */
+import com.android.future.usb.UsbAccessory;
+import com.android.future.usb.UsbManager;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class simpleTransrateADKActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = "Chapter02";
+
+    private static final String ACTION_USB_PERMISSION = "com.regaria.simpleTransrateADKActivity.action.USB_PERMISSION";
+
+    private UsbManager mUsbManager;
+    private PendingIntent mPermissionIntent;
+    private boolean mPermissionRequestPending;
+
+    UsbAccessory mAccessory;
+    ParcelFileDescriptor mFileDescriptor;
+    FileOutputStream mOutputStream;
+
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mUsbManager = UsbManager.getInstance(this);
+
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
+                ACTION_USB_PERMISSION), 0);
+
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+        registerReceiver(mUsbReceiver, filter);
+
         setContentView(R.layout.main);
+
+        Button btnLedOn = (Button) findViewById(R.id.button_on);
+        Button btnLedOff = (Button) findViewById(R.id.button_off);
+        btnLedOn.setOnClickListener(this);
+        btnLedOff.setOnClickListener(this);
     }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_on:
+                sendCommand(LED_COMMAND, LED_ON);
+                break;
+            case R.id.button_off:
+                sendCommand(LED_COMMAND, LED_OFF);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        if (mOutputStream != null) {
+            return;
+        }
+
+        UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+        UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+        if (accessory != null) {
+            if (mUsbManager.hasPermission(accessory)) {
+                openAccessory(accessory);
+            } else {
+                synchronized (mUsbReceiver) {
+                    if (!mPermissionRequestPending) {
+                        mUsbManager.requestPermission(accessory,
+                                mPermissionIntent);
+                        mPermissionRequestPending = true;
+                    }
+                }
+            }
+        } else {
+            Log.d(TAG, "mAccessory is null");
+        }    	
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        closeAccessory();
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mUsbReceiver);
+        super.onDestroy();
+    }
+
+    private void openAccessory(UsbAccessory accessory) {
+
+        mFileDescriptor = mUsbManager.openAccessory(accessory);
+        if (mFileDescriptor != null) {
+            mAccessory = accessory;
+            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+            mOutputStream = new FileOutputStream(fd);
+
+            Log.d(TAG, "accessory opened");
+        } else {
+            Log.d(TAG, "accessory open fail");
+        }
+    }
+
+    private void closeAccessory() {
+        try {
+            if (mFileDescriptor != null) {
+                mFileDescriptor.close();
+            }
+        } catch (IOException e) {
+        } finally {
+            mFileDescriptor = null;
+            mAccessory = null;
+        }
+    }
+
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbAccessory accessory = UsbManager.getAccessory(intent);
+                    if (intent.getBooleanExtra(
+                            UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        openAccessory(accessory);
+                    } else {
+                        Log.d(TAG, "permission denied for accessory "
+                                + accessory);
+                    }
+                    mPermissionRequestPending = false;
+                }
+            } else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
+                UsbAccessory accessory = UsbManager.getAccessory(intent);
+                if (accessory != null && accessory.equals(mAccessory)) {
+                    closeAccessory();
+                }
+            }
+        }
+    };
+
+    private final byte LED_COMMAND = 0x10;
+    private final byte LED_OFF = 0x00;
+    private final byte LED_ON = 0x01;
+
+    public void sendCommand(byte command, byte value) {
+
+        byte[] buffer = new byte[2];
+
+        buffer[0] = command;
+        buffer[1] = value;
+        if (mOutputStream != null) {
+            try {
+                mOutputStream.write(buffer);
+            } catch (IOException e) {
+                Log.e(TAG, "write failed", e);
+            }
+        }
+    }
+    
 }
